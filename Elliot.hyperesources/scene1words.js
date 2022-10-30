39ca78df5f13a8b1f75c43fcc943ea71030fa839
dreadphallinoid <117015576+dreadphallinoid@users.noreply.github.com>
2022-10-30 18:49:26 +0000

    004

--- Elliot.hyperesources/scene1words.js
+++
@@ -1,7 +0,0 @@
-var myData = {
-    "myEgg001" : "You are in a room. To the left you see something good. To the right, something else. Where would you like to go? You are in a room. To the left you see something good. To the right, something else. Where would you like to go? You are in a room. To the left you see something good. To the right, something else. Where would you like to go? You are in a room. To the left you see something good. To the right, something else. Where would you like to go? You are in a room. To the left you see something good. To the right, something else. Where would you like to go?",
-
-"myText002" : "completely different",
-
-"myColor" : "none"
-}
