52006e1f0ecf798beacfad6b508eb0d9cdc56e57
dreadphallinoid <117015576+dreadphallinoid@users.noreply.github.com>
2022-10-30 11:51:48 +0000

    test1

---
+++ Elliot.hyperesources/scene1words.js
@@ -0,0 +1,5 @@
+var myData = {
+    "myText" : "You are in a room. To the left you see something good. To the right, something else. Where would you like to go?",
+"myText2" : "completely different",
+    "myColor" : "rgb(0,8,15)"
+}
